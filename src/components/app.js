706052f1fd86436a643651cc588b0111035138d0
mistercmd <carson.deru@gmail.com>
2018-07-20 16:34:44 -0600

    Buttons styled, basic page styles added, attempting to place elements precisely on the page

--- src/components/app.js
+++ src/components/app.js
@@ -10,17 +10,25 @@ import HouseButtons from './ProductBtns/houseBtns';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar/>
+      <div className="home">
+        <div className="top">
+        <Navbar/> 
         <Products title="Model-3" productTitle="Model 3" productSubtitle="The car of the future" imageLink="https://i.imgur.com/wka19d9.jpg"/>
         <Model3Buttons/>
+        </div>
+        <div className="middle-top">
         <Products title="Model-S" productTitle="Model S" productSubtitle="The quickest car on the road" imageLink="https://i.imgur.com/ArqgT6B.jpg"/>
         <ModelSButtons/>
+        </div>
+        <div className="middle-bottom">
         <Products title="Model-X" productTitle="Model X" productSubtitle="The safest SUV ever" imageLink="https://i.imgur.com/erlyOVy.jpg"/>
         <ModelXButtons/>
+        </div>
+        <div className="bottom">
         <Products title="House" productTitle="Power Everything" productSubtitle="Solar and Powerwall" imageLink="https://i.imgur.com/HKX6UI7.jpg"/>
         <HouseButtons/>
         <Footer/>
+        </div>
       </div>
     );
   }
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -9,19 +9,16 @@ class Navbar extends Component {
                 </div>
 
                 <div className="navbar__vehicles">
-                    <ul className="navbar__vechicles__links">
-                        <li><a href="">Model S</a></li>
-                        <li><a href="">Model X</a></li>
-                        <li><a href="">Model 3</a></li>
-                        <li><a href="">Roadster</a></li>
-                        <li><a href="">Energy</a></li>
-
-                    </ul>
+                        <p><a href="">Model S</a></p>
+                        <p><a href="">Model X</a></p>
+                        <p><a href="">Model 3</a></p>
+                        <p><a href="">Roadster</a></p>
+                        <p><a href="">Energy</a></p>
                 </div>
 
                 <div className="navbar__info">
                 <ul className="navbar__info__links">
-                        <li><a href="" title="Get Tesla Updates">Get Newsletter</a></li>
+                        <li><a href="" title="Get Tesla">Get Newsletter</a></li>
                         <li><a href="">Shop</a></li>
                         <li><a href="">Sign In</a></li>
                         <li>{/*Insert fas fa bars*/}</li>
